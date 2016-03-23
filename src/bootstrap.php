f6779814f0cd4aaadaafa9039213a3b752174d52
Alexander Morland <alek.morland@gmail.com>
2016-03-23 21:53:57 +0100

    Whitespace formatting fixes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,13 +5,13 @@ namespace alkemann\h2l;
 require_once 'internals/functions.php';
 
 function handleError(\Exception $e, Request $request) {
-	if ($e instanceof \alkemann\h2l\exceptions\InvalidUrl) {
-		return (new Error(404, $e->getMessage()))->render();
-	}
-	if (DEBUG) {
-		echo $e->xdebug_message;
-		d($e, $request);
-	} else {
-		(new Error(500, $e->getMessage()))->render();
-	}
+    if ($e instanceof \alkemann\h2l\exceptions\InvalidUrl) {
+        return (new Error(404, $e->getMessage()))->render();
+    }
+    if (DEBUG) {
+        echo $e->xdebug_message;
+        d($e, $request);
+    } else {
+        (new Error(500, $e->getMessage()))->render();
+    }
 }
--- tests/unit/ErrorTest.php
+++ tests/unit/ErrorTest.php
@@ -4,44 +4,44 @@ namespace alkemann\h2l;
 
 class ErrorTest extends \PHPUnit_Framework_TestCase
 {
-	public function testConstructAndHeaderInjection()
-	{
+    public function testConstructAndHeaderInjection()
+    {
 
-		$header = null;
-		$e = new Error(406, "some text", function($h) use (&$header) {$header = $h; });
-		$this->assertTrue($e instanceof Response);
-		$this->assertTrue($e instanceof Error);
-		$e->render();
-		$this->assertEquals(406, $e->code);
-		$this->assertEquals('HTTP/1.0 406 Bad request', $header);
-	}
+        $header = null;
+        $e = new Error(406, "some text", function($h) use (&$header) {$header = $h; });
+        $this->assertTrue($e instanceof Response);
+        $this->assertTrue($e instanceof Error);
+        $e->render();
+        $this->assertEquals(406, $e->code);
+        $this->assertEquals('HTTP/1.0 406 Bad request', $header);
+    }
 
-	public function test400WithMessage()
-	{
-		$header = null;
-		$e = new Error(400, "This is message", function($h) use (&$header) { $header = $h; });
-		$this->assertEquals(400, $e->code);
-		$e->render();
-		$this->assertEquals('HTTP/1.0 400 This is message', $header);
-	}
+    public function test400WithMessage()
+    {
+        $header = null;
+        $e = new Error(400, "This is message", function($h) use (&$header) { $header = $h; });
+        $this->assertEquals(400, $e->code);
+        $e->render();
+        $this->assertEquals('HTTP/1.0 400 This is message', $header);
+    }
 
-	public function test404()
-	{
-		$header = null;
-		$e = new Error(404, "some text", function($h) use (&$header) {$header = $h; });
-		$e->render();
-		$this->assertEquals(404, $e->code);
-		$this->assertEquals('HTTP/1.0 404 Not Found', $header);
+    public function test404()
+    {
+        $header = null;
+        $e = new Error(404, "some text", function($h) use (&$header) {$header = $h; });
+        $e->render();
+        $this->assertEquals(404, $e->code);
+        $this->assertEquals('HTTP/1.0 404 Not Found', $header);
 
-	}
+    }
 
     /**
      * @expectedException Error
      */
-	public function testException()
-	{
-		$e = new Error(400, "This is message", 8);
-		$e->render();
-	}
+    public function testException()
+    {
+        $e = new Error(400, "This is message", 8);
+        $e->render();
+    }
 
 }
