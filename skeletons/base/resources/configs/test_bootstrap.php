7c422f5e30c121e5f8a80e7513c5a3d613e54ca0
Alexander Morland <alek.morland@gmail.com>
2016-03-24 01:10:52 +0100

    Set DEBUG to false in test bootstrap

--- skeletons/base/resources/configs/test_bootstrap.php
+++ skeletons/base/resources/configs/test_bootstrap.php
@@ -4,6 +4,7 @@ define('FIRST_APP_CONSTANT', 'FIRST_APP_CONSTANT');
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(dirname(dirname(__FILE__)))));
 define('VENDOR_PATH', ROOT . DS . 'vendor' . DS);
+define('DEBUG', false);
 
 require_once(VENDOR_PATH . 'autoload.php');
 require_once(VENDOR_PATH . 'alkemann/h2l/src/bootstrap.php');
