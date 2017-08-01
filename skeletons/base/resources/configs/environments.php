d9fd542b1840dbab22f8356666db08a725c7ca7a
Alexander Morland <alek.morland@gmail.com>
2017-08-01 22:25:01 +0200

    Updated skeletons to reflect changes

--- skeletons/base/resources/configs/environments.php
+++ skeletons/base/resources/configs/environments.php
@@ -3,17 +3,21 @@
 use alkemann\h2l\Environment;
 use alkemann\h2l\Log;
 
+$base = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'content' . DIRECTORY_SEPARATOR ;
+
 Environment::set([
     Environment::DEV => [
-        'debug' => true
-    ],
-    Environment::TEST => [
-        'debug' => false
+        'debug' => true,
+        'content_path' => $base . 'pages' . DIRECTORY_SEPARATOR,
+        'layout_path'  => $base . 'layouts' . DIRECTORY_SEPARATOR
     ],
     Environment::PROD => [
-        'debug' => false
+        'debug' => false,
+        'content_path' => $base . 'pages' . DIRECTORY_SEPARATOR,
+        'layout_path'  => $base . 'layouts' . DIRECTORY_SEPARATOR
     ]
 );
+
 
 // Check for server host and set enviroment here for example
 
