3cc318c10bdf44cf330f0083c3294fc7b6034423
Alexander Morland <alek.morland@gmail.com>
2017-08-09 09:52:33 +0200

    Fixed index.php for min skeleton

--- skeletons/min/webroot/index.php
+++ skeletons/min/webroot/index.php
@@ -1,7 +1,7 @@
 <?php
 // ***********
-$ROOT = realpath(dirname(dirname(__FILE__)));
-$VENDOR_PATH = $ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR;
+$ROOT = realpath(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR;
+$VENDOR_PATH = $ROOT . 'vendor' . DIRECTORY_SEPARATOR;
 // ***********
 
 require_once($VENDOR_PATH . 'autoload.php');
@@ -18,6 +18,8 @@ Environment::set([
         'content_path' => $ROOT . 'content' . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR,
     ]
 ], Environment::ALL);
+
+Router::alias('/', 'home.html');
 
 $request = new alkemann\h2l\Request($_REQUEST, $_SERVER, $_GET, $_POST);
 $response = $request->response();
