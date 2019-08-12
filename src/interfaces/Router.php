04f7faca4aaf419990f6c62b7a13ebda0b8a678e
Alexander Morland <alek.morland@gmail.com>
2019-08-12 16:40:37 +0200

    Fixed missing use statements in the Router interface

--- src/interfaces/Router.php
+++ src/interfaces/Router.php
@@ -2,6 +2,13 @@
 
 namespace alkemann\h2l\interfaces;
 
+use alkemann\h2l\util\Http;
+
+/**
+ * Interface Router
+ *
+ * @package alkemann\h2l\interfaces
+ */
 interface Router
 {
     public static function match(string $url, string $method = Http::GET): ?Route;
