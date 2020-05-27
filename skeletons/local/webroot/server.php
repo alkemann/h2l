26a010a454c77fc639038ecd3e82f4ff71765dbb
Alexander Morland <alkemann@users.noreply.github.com>
2020-05-27 16:57:25 +0200

    Namespace and scope in server test

--- skeletons/local/webroot/server.php
+++ skeletons/local/webroot/server.php
@@ -1,34 +1,38 @@
 <?php
 namespace local_server;
 
-if ($_SERVER['REQUEST_URI'] != '/' && file_exists($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'])) {
-    return false;
-}
+function put_url_in_request()
+{
+    $req = $_SERVER['REQUEST_URI'];
+    if ($req != '/' && file_exists($_SERVER['DOCUMENT_ROOT'] . $req)) {
+        return false;
+    }
 
-$_SERVER['PHP_SELF'] = '/index.php';
 
-$REQUEST_URI = $_SERVER['REQUEST_URI'];
-if (strpos($REQUEST_URI, '?')) {
-    $extract = function(string $REQUEST_URI): array {
-        list($REQUEST_URI, $things) = explode('?', $REQUEST_URI, 2);
-        $params = explode('&', $things);
-        $gets = [];
-        foreach ($params as $param) {
-            if (strpos($param, '=')) {
-                list($key, $value) = explode('=', $param, 2);
-            } else {
-                $key = $param;
-                $value = '';
+    $_SERVER['PHP_SELF'] = '/index.php';
+
+    $url = $_SERVER['REQUEST_URI'];
+    if (strpos($url, '?')) {
+        $extract = function(string $url): array {
+            list($url, $things) = explode('?', $url, 2);
+            $params = explode('&', $things);
+            $gets = [];
+            foreach ($params as $param) {
+                if (strpos($param, '=')) {
+                    list($key, $value) = explode('=', $param, 2);
+                } else {
+                    $key = $param;
+                    $value = '';
+                }
+                $gets[$key] = $value;
             }
-            $gets[$key] = $value;
-        }
-        return [$REQUEST_URI, $gets];
-    };
-    list($REQUEST_URI, $gets) = $extract($REQUEST_URI);
-    $_GET = $gets;
+            return [$url, $gets];
+        };
+        list($url, $gets) = $extract($url);
+        $_GET = $gets;
+    }
+
+    $_GET['url'] = $_REQUEST['url'] = $url;
 }
-
-$_GET['url'] = $_REQUEST['url'] = $REQUEST_URI;
-unset($REQUEST_URI);
-
+if (put_url_in_request() === false) return false;
 require 'index.php';
