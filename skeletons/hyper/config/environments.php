909a1d3e0454da399515e430b0a8b51dd4fe1c99
Alexander Morland <alek.morland@gmail.com>
2022-04-08 21:24:48 +0200

    Adding heroku bin only and hyper skeletons

---
+++ skeletons/hyper/config/environments.php
@@ -0,0 +1,38 @@
+<?php
+
+use alkemann\h2l\{Log, Environment, Router, Request, Response, util\Chain};
+
+$base = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR ;
+
+Environment::set([
+    Environment::DEV => [
+        'debug' => true,
+        'logs_path' => $base . 'resources' . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR,
+        'content_path' => $base . 'pages' . DIRECTORY_SEPARATOR,
+        'layout_path'  => $base . 'layouts' . DIRECTORY_SEPARATOR
+    ],
+    Environment::PROD => [
+        'debug' => false,
+        'logs_path' => $base . 'resources' . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR,
+        'content_path' => $base . 'pages' . DIRECTORY_SEPARATOR,
+        'layout_path'  => $base . 'layouts' . DIRECTORY_SEPARATOR
+    ]
+], Environment::ALL);
+
+Log::handler('file', [Log::class, 'file']);
+
+// Middleware to add a log response for request and what response handler is chosen
+Environment::addMiddle(
+    function(Request $request, Chain $chain): ?Response {
+        Log::debug("== REQUEST: {$request->method()} '{$request->url()}' ==");
+        $response = $chain->next($request);
+        if ($response) Log::debug("== Response Handler: " . get_class($response));
+        else Log::debug("== Null Response");
+        return $response;
+    },
+    Environment::ALL
+);
+
+Router::alias('/', 'home.html');
+
+// Check for server host and set environment here for example
---
+++ skeletons/hyper/layouts/default/head.html.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          // colors: {}
+        }
+      }
+    }
+  </script>
+</head>
+<body>
